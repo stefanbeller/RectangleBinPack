e6c0fa8d43f6f20e5fa6c338263f9c44c17f110a
Stefan Beller <stefanbeller@googlemail.com>
2012-09-29 17:00:56 +0200

    flare-game: Add conversions for the avatar equipment spritesheets
    Signed-off-by: Stefan Beller <stefanbeller@googlemail.com>

---
+++ flare/items.sh
@@ -0,0 +1,43 @@
+#!bin/sh
+
+# male fantasycore
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/buckler.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/buckler.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/clothes.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/clothes.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/dagger.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/dagger.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/greatbow.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/greatbow.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/greatstaff.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/greatstaff.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/greatsword.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/greatsword.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/head_bald.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/head_bald.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/head_hood.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/head_hood.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/head_short.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/head_short.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/leather_armor.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/leather_armor.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/longbow.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/longbow.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/longsword.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/longsword.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/rod.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/rod.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/shield.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/shield.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/shortbow.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/shortbow.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/shortsword.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/shortsword.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/slingshot.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/slingshot.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/staff.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/staff.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/steel_armor.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/steel_armor.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/wand.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/wand.png
+
+#female fantasycore
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/female/buckler.txt --images ../../flare-game/mods/fantasycore/images/avatar/female/buckler.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/female/clothes.txt --images ../../flare-game/mods/fantasycore/images/avatar/female/clothes.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/female/dagger.txt --images ../../flare-game/mods/fantasycore/images/avatar/female/dagger.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/female/greatbow.txt --images ../../flare-game/mods/fantasycore/images/avatar/female/greatbow.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/female/greatstaff.txt --images ../../flare-game/mods/fantasycore/images/avatar/female/greatstaff.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/female/greatsword.txt --images ../../flare-game/mods/fantasycore/images/avatar/female/greatsword.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/female/head_long.txt --images ../../flare-game/mods/fantasycore/images/avatar/female/head_long.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/female/leather_armor.txt --images ../../flare-game/mods/fantasycore/images/avatar/female/leather_armor.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/female/longbow.txt --images ../../flare-game/mods/fantasycore/images/avatar/female/longbow.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/female/longsword.txt --images ../../flare-game/mods/fantasycore/images/avatar/female/longsword.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/female/rod.txt --images ../../flare-game/mods/fantasycore/images/avatar/female/rod.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/female/shield.txt --images ../../flare-game/mods/fantasycore/images/avatar/female/shield.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/female/shortbow.txt --images ../../flare-game/mods/fantasycore/images/avatar/female/shortbow.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/female/shortsword.txt --images ../../flare-game/mods/fantasycore/images/avatar/female/shortsword.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/female/slingshot.txt --images ../../flare-game/mods/fantasycore/images/avatar/female/slingshot.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/female/staff.txt --images ../../flare-game/mods/fantasycore/images/avatar/female/staff.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/female/steel_armor.txt --images ../../flare-game/mods/fantasycore/images/avatar/female/steel_armor.png
+./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/female/wand.txt --images ../../flare-game/mods/fantasycore/images/avatar/female/wand.png
