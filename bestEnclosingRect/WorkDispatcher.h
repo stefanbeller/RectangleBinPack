3869bfcf24151175c673675e7224599ac1b1efa0
Stefan Beller <stefanbeller@googlemail.com>
2012-11-20 12:24:40 +0100

    WorkDispatcher: exchange public and private section in header.

--- bestEnclosingRect/WorkDispatcher.h
+++ bestEnclosingRect/WorkDispatcher.h
@@ -12,24 +12,6 @@
 #include <queue>
 
 class WorkDispatcher {
-private:
-	const unsigned MAX_THREADS; // how many threads to spawn.
-
-	// always aquire the semaphores with lowest priority first.
-	sem_t mutex;        // prio 3 (highest)
-	sem_t workingcount; // prio 2
-	sem_t freecount;    // prio 1 (lowest)
-
-	std::queue<void*> jobs; // queue holding the arguments to be solved.
-	pthread_t *threads; // threads
-
-	// runs threaded, calls getTask and the external function.
-	// argument is a WorkDispatcher instance to be served.
-	static void *dispatcher(void*);
-
-	void *getTask(); // returns the next argument for the function to be called.
-	void (*externcall)(void*); // stores the function to be called.
-
 public:
 	/**
 	 * Creates a WorkDispatcher, which uses up to \a maxthreads threads.
@@ -50,6 +32,24 @@ public:
 	 * from one thread.
 	 */
 	void addTask(void *task);
+
+private:
+	const unsigned MAX_THREADS; // how many threads to spawn.
+
+	// always aquire the semaphores with lowest priority first.
+	sem_t mutex;        // prio 3 (highest)
+	sem_t workingcount; // prio 2
+	sem_t freecount;    // prio 1 (lowest)
+
+	std::queue<void*> jobs; // queue holding the arguments to be solved.
+	pthread_t *threads; // threads
+
+	// runs threaded, calls getTask and the external function.
+	// argument is a WorkDispatcher instance to be served.
+	static void *dispatcher(void*);
+
+	void *getTask(); // returns the next argument for the function to be called.
+	void (*externcall)(void*); // stores the function to be called.
 };
 
 #endif
