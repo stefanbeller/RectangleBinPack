d444e361db54e8b8184d15547d513157fb99a662
Stefan Beller <stefanbeller@googlemail.com>
2013-04-03 12:25:10 +0200

    items.sh

--- flare/items.sh
+++ flare/items.sh
@@ -1,48 +1,13 @@
 #!bin/sh
 
-#~ female iron buckler
-#~ male iron buckler
-#~
-#~ stealth
-#~
-#~ bear_trap
-#~ caltrops
-#~ throw_axe
-#~ throw_knife
-
-./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/male/iron_buckler.txt --images ../../flare-game/mods/fantasycore/images/avatar/male/iron_buckler.png
-
-./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/avatar/female/iron_buckler.txt --images ../../flare-game/mods/fantasycore/images/avatar/female/iron_buckler.png
-
-
-./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/enemies/stealth.txt --images ../../flare-game/mods/fantasycore/images/enemies/stealth.png
-
-
-./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/powers/bear_trap.txt --images ../../flare-game/mods/fantasycore/images/powers/bear_trap.png
-
-./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/powers/caltrops.txt --images ../../flare-game/mods/fantasycore/images/powers/caltrops.png
-
-./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/powers/throw_axe.txt --images ../../flare-game/mods/fantasycore/images/powers/throw_axe.png
-
-./spritesheetpacker.py --definition ../../flare-game/mods/fantasycore/animations/powers/throw_knife.txt --images ../../flare-game/mods/fantasycore/images/powers/throw_knife.png
-
-
 # minicore
 
-
-./spritesheetpacker.py --resize --definition ../../flare-game/mods/minicore/animations/avatar/male/iron_buckler.txt --images ../../flare-game/mods/minicore/images/avatar/male/iron_buckler.png
-
-./spritesheetpacker.py --resize --definition ../../flare-game/mods/minicore/animations/avatar/female/iron_buckler.txt --images ../../flare-game/mods/minicore/images/avatar/female/iron_buckler.png
-
-
-./spritesheetpacker.py --resize --definition ../../flare-game/mods/minicore/animations/enemies/stealth.txt --images ../../flare-game/mods/minicore/images/enemies/stealth.png
-
-
-./spritesheetpacker.py --resize --definition ../../flare-game/mods/minicore/animations/powers/bear_trap.txt --images ../../flare-game/mods/minicore/images/powers/bear_trap.png
-
-./spritesheetpacker.py --resize --definition ../../flare-game/mods/minicore/animations/powers/caltrops.txt --images ../../flare-game/mods/minicore/images/powers/caltrops.png
-
-./spritesheetpacker.py --resize --definition ../../flare-game/mods/minicore/animations/powers/throw_axe.txt --images ../../flare-game/mods/minicore/images/powers/throw_axe.png
-
-./spritesheetpacker.py --resize --definition ../../flare-game/mods/minicore/animations/powers/throw_knife.txt --images ../../flare-game/mods/minicore/images/powers/throw_knife.png
+./spritesheetpacker.py --resize --definition ../../flare-game/mods/minicore/animations/npcs/guild_man1.txt --images ../../flare-game/mods/minicore/images/npcs/guild_man1.png
+./spritesheetpacker.py --resize --definition ../../flare-game/mods/minicore/animations/npcs/guild_man2.txt --images ../../flare-game/mods/minicore/images/npcs/guild_man2.png
+./spritesheetpacker.py --resize --definition ../../flare-game/mods/minicore/animations/npcs/peasant_man1.txt --images ../../flare-game/mods/minicore/images/npcs/peasant_man1.png
+./spritesheetpacker.py --resize --definition ../../flare-game/mods/minicore/animations/npcs/peasant_man2.txt --images ../../flare-game/mods/minicore/images/npcs/peasant_man2.png
+./spritesheetpacker.py --resize --definition ../../flare-game/mods/minicore/animations/npcs/peasant_woman1.txt --images ../../flare-game/mods/minicore/images/npcs/peasant_woman1.png
+./spritesheetpacker.py --resize --definition ../../flare-game/mods/minicore/animations/npcs/peasant_woman2.txt --images ../../flare-game/mods/minicore/images/npcs/peasant_woman2.png
+./spritesheetpacker.py --resize --definition ../../flare-game/mods/minicore/animations/npcs/wandering_trader1.txt --images ../../flare-game/mods/minicore/images/npcs/wandering_trader1.png
+./spritesheetpacker.py --resize --definition ../../flare-game/mods/minicore/animations/npcs/wandering_trader2.txt --images ../../flare-game/mods/minicore/images/npcs/wandering_trader2.png
 
